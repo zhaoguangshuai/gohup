d9f3bb86b6b78f6bfbd251936d54c25f90362c3d
赵光帅 <969426998@qq.com>
2024-02-25 22:03:02 +0800

    配置方案的实现

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"gohub/bootstrap"
+	btsConfig "gohub/config"
+	"gohub/pkg/config"
 
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	// 加载 config 目录下的配置信息
+	btsConfig.Initialize()
+}
+
 func main() {
+	// 配置初始化，依赖命令行 --env 参数
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
+
 	// 初始化 Gin 实例
 	router := gin.New()
 
@@ -15,7 +29,7 @@ func main() {
 	bootstrap.SetupRoute(router)
 
 	// 运行服务，默认为 8080，我们指定端口为 8000
-	err := router.Run(":3000")
+	err := router.Run(":" + config.Get("app.port"))
 	if err != nil {
 		// 错误处理，端口被占用了或者其他错误
 		fmt.Println(err.Error())
