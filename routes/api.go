0975af6e5540242853aaa223071f8f8b00b0526d
赵光帅 <969426998@qq.com>
2024-02-26 20:49:23 +0800

    添加手机是否注册接口

--- routes/api.go
+++ routes/api.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"gohub/app/http/controllers/api/v1/auth"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,13 +11,12 @@ func RegisterAPIRoutes(r *gin.Engine) {
 	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
 	v1 := r.Group("/v1")
 	{
-		// 注册一个路由
-		v1.GET("/", func(c *gin.Context) {
-			// 以 JSON 格式响应
-			c.JSON(http.StatusOK, gin.H{
-				"Hello": "World!",
-			})
-		})
+		authGroup := v1.Group("/auth")
+		{
+			suc := new(auth.SignupController)
+			// 判断手机是否已注册
+			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
+		}
 	}
 	v2 := r.Group("/v2")
 	{
