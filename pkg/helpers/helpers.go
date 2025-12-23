b916c49ad803c5c2402fc74d45c32f7fe0d92227
L-赵光帅 <l-zhaoguangshuai@qianxin.com>
2025-12-23 14:34:27 +0800

    key 命令

--- pkg/helpers/helpers.go
+++ pkg/helpers/helpers.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"reflect"
 	"time"
+	mathrand "math/rand"
 )
 
 // Empty 类似于 PHP 的 empty() 函数
@@ -60,4 +61,15 @@ func FirstElement(args []string) string {
         return args[0]
     }
     return ""
+}
+
+// RandomString 生成长度为 length 的随机字符串
+func RandomString(length int) string {
+    mathrand.Seed(time.Now().UnixNano())
+    letters := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    b := make([]byte, length)
+    for i := range b {
+        b[i] = letters[mathrand.Intn(len(letters))]
+    }
+    return string(b)
 }
